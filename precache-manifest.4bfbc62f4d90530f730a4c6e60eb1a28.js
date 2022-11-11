bd2b21188abb846422987f47b26fdf5cdfc10b65
baroxFPTU <baopqse160018@fpt.edu.vn>
2022-11-11 09:11:00 +0700

    Updates

---
+++ precache-manifest.4bfbc62f4d90530f730a4c6e60eb1a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af1d76453de6e8126550",
+    "url": "/food-recipe-frontend/static/css/main.b7e9b838.chunk.css"
+  },
+  {
+    "revision": "af1d76453de6e8126550",
+    "url": "/food-recipe-frontend/static/js/main.af1d7645.chunk.js"
+  },
+  {
+    "revision": "6a9344cc9cf24032e607",
+    "url": "/food-recipe-frontend/static/css/1.0c461217.chunk.css"
+  },
+  {
+    "revision": "6a9344cc9cf24032e607",
+    "url": "/food-recipe-frontend/static/js/1.6a9344cc.chunk.js"
+  },
+  {
+    "revision": "aede7269d47e4ef2dbe3",
+    "url": "/food-recipe-frontend/static/js/runtime~main.aede7269.js"
+  },
+  {
+    "revision": "2570848d90fc51cfde396f06d75bbac5",
+    "url": "/food-recipe-frontend/index.html"
+  }
+];
